5ab46a8acc4c6c00dee44a7ee11b7e062e58e681
christianluiz123arrobacr-lab <christianluiz123arrobacr@gmail.com>
2026-04-06 20:49:25 -0300

    Update trpc.ts

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,8 +1,20 @@
-import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "../server/routers";
 import { createContext } from "../server/_core/context";
 
-export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-});
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req: Request) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () =>
+      createContext({
+        req: req as any,
+        res: {} as any,
+      }),
+  });
+}
