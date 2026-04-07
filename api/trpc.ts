d111ec363224aa392a1c7ed7ea484500102937b9
christianluiz123arrobacr-lab <christianluiz123arrobacr@gmail.com>
2026-04-06 23:40:50 -0300

    Update TRPC handler to use new HTTP adapter

--- api/trpc.ts
+++ api/trpc.ts
@@ -1,5 +1,6 @@
-import { appRouter } from "../server/routers.ts";
-import { createContext } from "../server/_core/context.ts";
+import { createHTTPHandler } from "@trpc/server/adapters/node-http";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
 
 const handler = createHTTPHandler({
   router: appRouter,
